8df65074ceb5e663b239844931f159c3a27f0980
vergaraCoder <jhonatanvergara072@gmail.com>
2024-11-25 18:14:14 -0500

    feat

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FilterError } from './common/erros/filter/filter.exception';
 import { ValidationPipe } from '@nestjs/common';
+import express from 'express';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new FilterError());
   app.enableCors({
     origin: '*', 
